9e0bcf64469b432ecd5d04cc0ef96b5f29af0efb
Will Kirwin <will.kirwin@gmail.com>
2015-10-01 17:34:13 -0600

    put correct install scripts in dev/install for basic provisioning

--- dev/install/install-03-ipython-nbserver.sh
+++
@@ -1,3 +0,0 @@
-sudo -u vagrant IPYTHONDIR=/home/vagrant/.ipython /home/vagrant/anaconda/bin/ipython profile create
-sudo -u vagrant IPYTHONDIR=/home/vagrant/.ipython /home/vagrant/anaconda/bin/ipython profile create nbserver
-sudo -u vagrant cp -f /vagrant/resources/ipython_notebook_config.py /home/vagrant/.ipython/profile_nbserver

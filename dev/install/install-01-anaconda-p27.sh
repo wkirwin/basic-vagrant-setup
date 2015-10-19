35e6f67f4621e6bb7f06653b0fc68a81b4490e45
Will Kirwin <will.kirwin@gmail.com>
2015-10-19 17:25:44 -0600

    fixed a type in install-01-anaconda-p27.sh

--- dev/install/install-01-anaconda-p27.sh
+++ dev/install/install-01-anaconda-p27.sh
@@ -1,6 +1,6 @@
 # chmod a+x /home/vagrant/local/vagrant_provision/Anaconda-2.3.0-Linux-x86_64.sh
 ANACONDA_FILE = '/vagrant/resources/Anaconda-2.3.0-Linux-x86_64.sh'
-if [ ! -f $FILE ];
+if [ ! -f $ANACONDA_FILE ];
 then
     wget -P /vagrant/resources/ https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 fi

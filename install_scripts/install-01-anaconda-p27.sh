60a7a06a843a3b026d75c3e790659b0a4a130fbb
Will Kirwin <will.kirwin@gmail.com>
2016-05-10 16:35:50 -0600

    fixed py27 install script

--- install_scripts/install-01-anaconda-p27.sh
+++ install_scripts/install-01-anaconda-p27.sh
@@ -2,13 +2,13 @@
 
 su vagrant
 
-ANACONDA_FILE = 'Anaconda2-2.4.1-Linux-x86_64.sh'
-if [ ! -f /vagrant/resources/$ANACONDA_FILE ];
+export ANACONDA_FILE="Anaconda2-4.0.0-Linux-x86_64.sh"
+if [ ! -f /vagrant/resources/${ANACONDA_FILE} ];
 then
-    wget -P /vagrant/resources/ https://repo.continuum.io/archive/$ANACONDA_FILE
+    wget -P /vagrant/resources/ https://repo.continuum.io/archive/${ANACONDA_FILE}
 fi
 
-bash $ANACONDA_FILE -b -p /home/vagrant/anaconda
+bash /vagrant/resources/$ANACONDA_FILE -b -p /home/vagrant/anaconda
 echo "export PATH=/home/vagrant/anaconda/bin:\$PATH" >> /home/vagrant/.zshrc
 echo "export PATH=/home/vagrant/anaconda/bin:\$PATH" >> /home/vagrant/.bashrc
 
